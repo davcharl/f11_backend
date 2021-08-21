305c1382bed57db6c6cdb6e361a5300cdb9a41df
unknown <davidc.2107@gmail.com>
2021-08-21 20:36:45 +0100

    Updated initial SQL scripts.

---
+++ form11_backend_test/sql_scripts/02-tax_payer.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE  IF NOT EXISTS `dc_tax_payer`;
+USE `dc_tax_payer`;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `taxpayer`;
+
+CREATE TABLE `taxpayer` (
+  `ppsn_id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` nvarchar(45) NOT NULL,
+  `last_name` nvarchar(45) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `email` nvarchar(50) DEFAULT NULL,
+  `xml_location` nvarchar(400) DEFAULT NULL,
+  PRIMARY KEY (`ppsn_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
